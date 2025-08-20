2da5ee17d9806f6c4059f2739757df842227784e
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-08-20 12:49:18 +0530

    File updated at 8/20/2025, 12:49:18 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,16 +1,48 @@
-// Last updated: 8/20/2025, 12:18:51 PM
+// Last updated: 8/20/2025, 12:49:18 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.*;
+
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int i=0;i<k;i++){
-            pq.add(nums[i]);
-        }
-        for(int i=k;i<nums.length;i++){
-            if(nums[i]>pq.peek()){
-                pq.poll();
-                pq.add(nums[i]);
+    public ListNode mergeKLists(ListNode[] lists) {
+        // Min-heap based on node values
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+        // Dummy head to simplify result list creation
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        // Add the head of each list to the priority queue
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                pq.add(lists[i]);
             }
         }
-        return pq.peek();
+
+        // Build the merged linked list
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            tail.next = node;
+            tail = tail.next;
+
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+
+        return dummy.next;
     }
 }
