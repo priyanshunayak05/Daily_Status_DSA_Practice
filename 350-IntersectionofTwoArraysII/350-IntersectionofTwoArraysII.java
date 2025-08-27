73405b1b05a87a755c9ac9afd46ccf28b4f60d44
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-08-27 19:35:57 +0530

    Create 350-IntersectionofTwoArraysII

---
+++ 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.java
@@ -0,0 +1,26 @@
+// Last updated: 8/27/2025, 7:35:57 PM
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])){
+                map.put(nums1[i],map.get(nums1[i])+1);
+            }
+            else{
+                map.put(nums1[i],1);
+            }
+        }
+        List<Integer>ll=new ArrayList<>();
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i])>0){
+                ll.add(nums2[i]);
+                map.put(nums2[i],map.get(nums2[i])-1);
+            }
+        }
+        int [] ans=new int[ll.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=ll.get(i);
+        }
+        return ans;
+    }
+}
