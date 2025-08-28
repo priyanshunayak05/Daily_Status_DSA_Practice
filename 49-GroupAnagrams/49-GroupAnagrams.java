bf8018b1211e0fad3ee8fa9de6ae94ec01740b2f
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-08-28 12:08:58 +0530

    File updated at 8/28/2025, 12:08:57 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,30 +1,65 @@
-// Last updated: 8/28/2025, 11:22:04 AM
+// Last updated: 8/28/2025, 12:08:57 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map=new HashMap<>();
-        for(int i=0;i<strs.length;i++){
-            String key=GetKey(strs[i]);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
-            }
-            map.get(key).add(strs[i]);
-        }
-        List<List<String>> ll=new ArrayList<>();
-        for(String key:map.keySet()){
-            ll.add(map.get(key));
-        }
-        return ll;
+    class verticalPair{
+        TreeNode node;
+        int l;
+        int v;
+        verticalPair(TreeNode node,int l,int v){
+            this.l=l;
+            this.v=v;
+            this.node=node;
+        }  
     }
-    public static String GetKey(String s){
-        int[] freq=new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            freq[ch-'a']++;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Queue<verticalPair> q=new LinkedList<>();
+        TreeMap<Integer,List<verticalPair>> map=new TreeMap<>();
+        q.add(new verticalPair(root,0,0));
+        while(!q.isEmpty()){
+            verticalPair vp=q.poll();
+            if(!map.containsKey(vp.v)){
+                map.put(vp.v,new ArrayList<>());
+            }
+            map.get(vp.v).add(vp);
+            if(vp.node.left!=null){
+                q.add(new verticalPair(vp.node.left,vp.l+1,vp.v-1));
+            }
+            if(vp.node.right!=null){
+                q.add(new verticalPair(vp.node.right,vp.l+1,vp.v+1));
+            }
         }
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<freq.length;i++){
-            sb.append(freq[i]).append(" ");
+        List<List<Integer>> ans =new ArrayList<>();
+        for(int key:map.keySet()){
+            List<verticalPair> ll=map.get(key);
+            Collections.sort(ll,new Comparator<verticalPair>(){
+                @Override
+                public int compare(verticalPair o1,verticalPair o2){
+                    if(o1.l==o2.l){
+                        return o1.node.val-o2.node.val;
+                    }
+                    return 0;
+                }
+            });
+            List<Integer> list=new ArrayList<>();
+            for(verticalPair v:ll){
+                list.add(v.node.val);
+            }
+            ans.add(list);
         }
-        return sb.toString();
+        return ans;
     }
 }
