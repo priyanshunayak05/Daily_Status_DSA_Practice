60a7185de42219026a0fb15ed07a97947e157250
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-08-28 19:02:39 +0530

    Create 653-TwoSumIV-InputisaBST

---
+++ 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.java
@@ -0,0 +1,50 @@
+// Last updated: 8/28/2025, 7:02:38 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> res=new ArrayList<>();
+        Inorder(root,res);
+        int i=0;
+        int j=res.size()-1;
+        while(i<j){
+            int sum=(res.get(i)+res.get(j));
+            if(sum==k){
+                return true;
+            }
+            else if (sum < k){
+                i++;
+            }
+            else{
+                j--;
+            }
+           
+        }
+        return false;
+
+        
+    }
+    public static void Inorder(TreeNode root,List<Integer>res){
+        if(root ==null){
+            return;
+        }
+        Inorder(root.left,res);
+        res.add(root.val);
+        Inorder(root.right,res);
+
+    }
+
+}
