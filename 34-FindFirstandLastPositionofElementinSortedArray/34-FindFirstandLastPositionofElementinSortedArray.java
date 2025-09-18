e03321dc492ba94c4dbd568c81c0bdc5d2b37fce
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-09-18 17:59:17 +0530

    File updated at 9/18/2025, 5:59:17 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,12 +1,20 @@
-// Last updated: 9/18/2025, 4:57:41 PM
+// Last updated: 9/18/2025, 5:59:17 PM
 class Solution {
-    public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return true;
+    public int findMin(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<right){
+            int mid= left+(right-left)/2;
+
+            if(nums[mid]>nums[right]){
+                left=mid+1;
+            }
+            else{
+                right=mid;
             }
         }
-        return false;
+        return nums[left];
         
     }
 }
