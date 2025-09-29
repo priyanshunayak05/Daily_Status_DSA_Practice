e61656680d04822be1478ed6efe9a3ef565b02a8
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-09-29 23:32:45 +0530

    File updated at 9/29/2025, 11:32:46 PM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,29 +1,16 @@
-// Last updated: 9/29/2025, 9:08:25 PM
+// Last updated: 9/29/2025, 11:32:46 PM
 class Solution {
-    public int numIslands(char[][] grid) {
-        if(grid==null || grid.length ==0 || grid[0].length==0){
-            return 0;
-        }
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
-                    count++;
-                }
+    public int maxProduct(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int pro=1;
+            for(int j=i;j<nums.length;j++){
+                pro *=nums[j];
+                max =Math.max(max,pro);
             }
+            
         }
-        return count;
-    }
-    public static void dfs(char [][]grid,int i,int j){
-        if(i<0 || i>=grid.length || j<0 ||j>=grid[0].length || grid[i][j]=='0'){
-            return;
-        }
-        grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
- 
+        return max;
+
     }
 }
