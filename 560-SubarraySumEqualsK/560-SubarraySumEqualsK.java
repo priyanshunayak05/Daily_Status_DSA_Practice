a3a0b3f6243d715c395fb14e4402707ded88f4af
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-09-29 14:07:00 +0530

    File updated at 9/29/2025, 2:07:00 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,29 +1,14 @@
-// Last updated: 8/1/2025, 1:56:11 PM
-import java.util.HashMap;
-import java.util.Map;
-
-public class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int prefixSum = 0;
-        Map<Integer, Integer> prefixCount = new HashMap<>();
-        
-        // Initialize with prefix sum of 0 having one count
-        prefixCount.put(0, 1);
-        
-        for (int num : nums) {
-            // Update the prefix sum
-            prefixSum += num;
-            
-            // Check if there exists a prefix sum that, when subtracted from current, equals k
-            if (prefixCount.containsKey(prefixSum - k)) {
-                count += prefixCount.get(prefixSum - k);
+// Last updated: 9/29/2025, 2:07:00 PM
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int c=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] <0){
+                    c++;
+                }
             }
-            
-            // Add the current prefix sum to the map
-            prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
         }
-        
-        return count;
+        return c;
     }
 }
