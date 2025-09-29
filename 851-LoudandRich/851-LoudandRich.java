1ff3f2abefce77531a65c880b300791df8544083
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-09-29 14:38:54 +0530

    File updated at 9/29/2025, 2:38:53 PM

--- 851-LoudandRich/851-LoudandRich.java
+++ 851-LoudandRich/851-LoudandRich.java
@@ -1,15 +1,21 @@
-// Last updated: 9/29/2025, 2:22:30 PM
+// Last updated: 9/29/2025, 2:38:53 PM
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int max=0;
-        for(int i=0;i<accounts.length;i++){
-            int sum=0;
-            for(int j=0;j<accounts[0].length;j++){
-                sum +=accounts[i][j];
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix[0].length;i++){
+            for(int j=i+1;j<matrix.length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
-            max=Math.max(max,sum);
         }
-        return max;
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
         
     }
 }
