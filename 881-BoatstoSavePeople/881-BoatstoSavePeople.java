a102b8e1d06e54b71fa7d371e50a0c22f3c758d8
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-10-02 01:49:59 +0530

    Create 881-BoatstoSavePeople

---
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -0,0 +1,23 @@
+// Last updated: 10/2/2025, 1:49:59 AM
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int i=0;
+        int c=0;
+        int j=people.length-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                c++;
+                i++;
+                j--;
+            }
+            else{
+                j--;
+                c++;
+            }
+        }
+            
+        return c;
+    }
+}
