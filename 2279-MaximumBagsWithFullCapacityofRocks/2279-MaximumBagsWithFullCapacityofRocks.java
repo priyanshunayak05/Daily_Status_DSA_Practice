28a656657670112e433d4795f1c3327700c35c7d
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-10-10 23:21:05 +0530

    Create 2279-MaximumBagsWithFullCapacityofRocks

---
+++ 2279-MaximumBagsWithFullCapacityofRocks/2279-MaximumBagsWithFullCapacityofRocks.java
@@ -0,0 +1,26 @@
+// Last updated: 10/10/2025, 11:21:04 PM
+class Solution {
+    public int maximumBags(int[] cap, int[] r, int a) {
+        int n=r.length;
+        int []diff=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++){
+            diff[i]=cap[i]-r[i];
+        }
+        Arrays.sort(diff);
+        for(int i=0;i<n;i++){
+            if(diff[i]==0){
+                c++;
+            }
+            else if(a>=diff[i]){
+                a-=diff[i];
+                c++;
+            }
+            else{
+                break;
+            }
+        }
+        return c;
+        
+    }
+}
