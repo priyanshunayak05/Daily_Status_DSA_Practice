bcb717bf66bd584184c91448ecfb7ac416298659
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-10-30 17:58:35 +0530

    Create 547-NumberofProvinces

---
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -0,0 +1,25 @@
+// Last updated: 10/30/2025, 5:58:34 PM
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        int prov=0;
+        boolean []visited=new boolean[n];
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(isConnected,visited,i);
+                prov++;
+            }
+        }
+        return prov;
+    }
+    public static void dfs(int [][]isConnected,boolean []visited,int i){
+        visited[i]=true;
+
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1 && !visited[j]){
+                dfs(isConnected,visited,j);
+            }
+        }
+    }
+}
