14f130fa13d304a86fd400b876069e44a239b1df
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-11-04 23:39:37 +0530

    File updated at 11/4/2025, 11:39:35 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,27 +1,44 @@
-// Last updated: 8/1/2025, 1:58:38 PM
-import java.util.Stack;
-
+// Last updated: 11/4/2025, 11:39:35 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c); // Push opening brackets onto the stack
-            } else {
-                if (stack.isEmpty()) {
-                    return false; // Closing bracket without an opening bracket
-                }
-                
-                char top = stack.pop();
-                if ((c == ')' && top != '(') || 
-                    (c == '}' && top != '{') || 
-                    (c == ']' && top != '[')) {
-                    return false; // Mismatched pair
-                }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp=head;
+        int cnt=0;
+        //check if k node eist
+        while(cnt<k){
+            if(temp==null){
+                return head;
             }
+            temp=temp.next;
+            cnt++;
+            
         }
+        // Resursive call for rest LL
+        ListNode prev=reverseKGroup(temp,k);
+
+
+        //Reverse the current node
+        temp=head;
+        cnt=0;
+        while(cnt<k){
+            ListNode next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+
+            cnt++;
+
+        }
+        return prev;
         
-        return stack.isEmpty(); // If stack is empty, all brackets matched correctly
     }
 }
