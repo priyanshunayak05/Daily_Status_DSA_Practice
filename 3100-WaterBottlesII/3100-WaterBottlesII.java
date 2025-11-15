0e16aef0d264c07f605befdc220b9d08244b498f
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-11-15 15:08:18 +0530

    File updated at 11/15/2025, 3:08:14 PM

--- 3100-WaterBottlesII/3100-WaterBottlesII.java
+++ 3100-WaterBottlesII/3100-WaterBottlesII.java
@@ -1,19 +1,16 @@
-// Last updated: 11/15/2025, 3:02:51 PM
+// Last updated: 11/15/2025, 3:08:14 PM
 class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-
-        int empty=numBottles;
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
         int total=numBottles;
+        int empty=numBottles;
 
         while(empty>=numExchange){
             empty -=numExchange;
             total++;
+            numExchange++;
             empty++;
 
         }
-
         return total;
-        
-        
     }
 }
