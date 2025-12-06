da8d3181981eb6d8b9ddcc70c8526a53e2fbe582
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:57:04 +0530

    Create 1268-MarketAnalysisI

---
+++ 1268-MarketAnalysisI/1268-MarketAnalysisI.sql
@@ -0,0 +1,12 @@
+-- Last updated: 12/6/2025, 11:57:01 PM
+# Write your MySQL query statement below
+
+
+SELECT u.user_id AS buyer_id, join_date, 
+IFNULL(COUNT(order_date), 0) AS orders_in_2019 
+FROM Users as u
+LEFT JOIN
+Orders as o
+ON u.user_id = o.buyer_id
+AND YEAR(order_date) = '2019'
+GROUP BY u.user_id
