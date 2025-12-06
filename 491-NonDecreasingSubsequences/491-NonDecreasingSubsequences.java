43acc9e82597c16694cef3ac8798ad27fa6ce415
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:58:43 +0530

    Create 491-NonDecreasingSubsequences

---
+++ 491-NonDecreasingSubsequences/491-NonDecreasingSubsequences.java
@@ -0,0 +1,28 @@
+// Last updated: 12/6/2025, 11:58:40 PM
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        
+        Set<List<Integer>>list=new HashSet<>();
+        List<Integer>ans=new ArrayList<>();
+        Non(nums,0,list,ans);
+        return new ArrayList(list);
+        
+    }
+    public static void Non(int[] nums,int idx,Set<List<Integer>>list,List<Integer>ans){
+        if(idx==nums.length){
+            if(ans.size()>=2){
+             list.add(new ArrayList(ans));
+        }
+        return;
+
+        }
+        
+    
+        if(ans.isEmpty() || nums[idx]>=ans.get(ans.size()-1)){
+            ans.add(nums[idx]);
+            Non(nums,idx+1,list,ans);
+            ans.remove(ans.size()-1);
+        }
+        Non(nums,idx+1,list,ans);
+    }
+}
