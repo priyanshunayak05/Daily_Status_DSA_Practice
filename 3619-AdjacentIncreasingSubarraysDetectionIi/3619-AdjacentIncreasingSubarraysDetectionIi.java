9a6930fb5282e5971551f49db3584ae34583b470
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:16 +0530

    Create 3619-AdjacentIncreasingSubarraysDetectionIi

---
+++ 3619-AdjacentIncreasingSubarraysDetectionIi/3619-AdjacentIncreasingSubarraysDetectionIi.java
@@ -0,0 +1,42 @@
+// Last updated: 12/6/2025, 11:56:13 PM
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+    int n = nums.size();
+    int[] inc = new int[n];
+    inc[n - 1] = 1;
+
+    
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums.get(i) < nums.get(i + 1)) {
+            inc[i] = inc[i + 1] + 1;
+        } else {
+            inc[i] = 1;
+        }
+    }
+
+    int left = 1, right = n / 2;
+    int ans = 0;
+
+   
+    while (left <= right) {
+        int k = left + (right - left) / 2;
+        boolean found = false;
+
+        for (int i = 0; i + 2 * k - 1 < n; i++) {
+            if (inc[i] >= k && inc[i + k] >= k) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            ans = k;       
+            left = k + 1;  
+        } else {
+            right = k - 1; 
+        }
+    }
+
+    return ans;
+}
+}
