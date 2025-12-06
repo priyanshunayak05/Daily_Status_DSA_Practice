052080c67e40b2f80448848448791e2e1c315f0d
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:10 +0530

    Create 4116-MinimumMovesToEqualArrayElementsIii

---
+++ 4116-MinimumMovesToEqualArrayElementsIii/4116-MinimumMovesToEqualArrayElementsIii.java
@@ -0,0 +1,22 @@
+// Last updated: 12/6/2025, 11:56:07 PM
+class Solution {
+    public int minMoves(int[] nums) {
+        int max=0;
+        int n=nums.length;
+        for(int num:nums){
+            max=Math.max(max,num);
+        }
+
+
+        int sum=0;
+        for(int num:nums){
+            sum +=max-num;
+        }
+
+
+        return sum;
+
+
+        
+    }
+}
