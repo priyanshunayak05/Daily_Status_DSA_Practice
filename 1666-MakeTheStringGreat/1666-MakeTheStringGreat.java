bcc0b55f8c8b8122b581268a7f615dcc33d29aa6
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:44 +0530

    Create 1666-MakeTheStringGreat

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.java
@@ -0,0 +1,17 @@
+// Last updated: 12/6/2025, 11:56:41 PM
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            int len=sb.length();
+            if(len>0 && Math.abs(sb.charAt(len-1)-ch)==32){
+                sb.deleteCharAt(len-1);
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+
+    }
+}
