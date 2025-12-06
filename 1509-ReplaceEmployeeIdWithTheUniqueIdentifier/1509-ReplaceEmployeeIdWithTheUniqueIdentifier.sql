c61f2a7a633aa38142b028c93998b066b72d1702
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:51 +0530

    Create 1509-ReplaceEmployeeIdWithTheUniqueIdentifier

---
+++ 1509-ReplaceEmployeeIdWithTheUniqueIdentifier/1509-ReplaceEmployeeIdWithTheUniqueIdentifier.sql
@@ -0,0 +1,6 @@
+-- Last updated: 12/6/2025, 11:56:49 PM
+# Write your MySQL query statement below
+SELECT euni.unique_id, e.name 
+FROM Employees e  
+LEFT JOIN EmployeeUNI euni  
+ON e.id = euni.id;
