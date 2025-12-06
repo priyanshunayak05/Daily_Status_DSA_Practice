26bfe36d6591268eb3bcc8e3b2d8dc7fa9e7b68a
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:29 +0530

    Create 2387-PartitionArraySuchThatMaximumDifferenceIsK

---
+++ 2387-PartitionArraySuchThatMaximumDifferenceIsK/2387-PartitionArraySuchThatMaximumDifferenceIsK.java
@@ -0,0 +1,18 @@
+// Last updated: 12/6/2025, 11:56:26 PM
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int c=1;
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]-prev >k){
+                c++;
+                prev=nums[i];
+            }
+                
+        }
+        return c;
+        
+    }
+}
