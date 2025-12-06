1f7cc89eeaa6620daa6b08bd6724a6566f78846d
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-06 23:56:09 +0530

    Create 4074-CountSubarraysWithMajorityElementI

---
+++ 4074-CountSubarraysWithMajorityElementI/4074-CountSubarraysWithMajorityElementI.java
@@ -0,0 +1,21 @@
+// Last updated: 12/6/2025, 11:56:06 PM
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int[] dresaniel = nums; 
+        
+        int n = dresaniel.length;
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int targetCount = 0;
+            for (int j = i; j < n; j++) {
+                if (dresaniel[j] == target) targetCount++;
+                
+                int len = j - i + 1;
+                if (targetCount > len / 2) count++;
+            }
+        }
+        
+        return count;
+    }
+}
