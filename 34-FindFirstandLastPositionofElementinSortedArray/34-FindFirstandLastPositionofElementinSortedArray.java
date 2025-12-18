ce3dff8f69dea38f0faab55336eb0c3b0f7ff713
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-18 22:21:49 +0530

    File updated at 12/18/2025, 10:21:48 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,20 +1,49 @@
-// Last updated: 9/18/2025, 5:59:17 PM
-class Solution {
-    public int findMin(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-
-        while(left<right){
-            int mid= left+(right-left)/2;
-
-            if(nums[mid]>nums[right]){
-                left=mid+1;
-            }
-            else{
-                right=mid;
-            }
-        }
-        return nums[left];
-        
-    }
-}
+// Last updated: 12/18/2025, 10:21:48 PM
+1class Solution {
+2    public int[] searchRange(int[] nums, int target) {
+3        int f=first(nums,target);
+4        int l=last(nums,target);
+5        return new int[]{f,l};
+6
+7    }
+8    public static int first(int []nums,int target){
+9        int l=0;
+10        int r=nums.length-1;
+11        int index=-1;
+12        while(l<=r){
+13            int mid=l+(r-l)/2;
+14
+15              if(nums[mid]==target){
+16                index=mid;
+17            }
+18            if(nums[mid]>=target){
+19                r=mid-1;
+20            }
+21            else{
+22                l=mid+1;
+23            }
+24          
+25        }
+26        return index;
+27    }
+28     public static int last(int []nums,int target){
+29        int l=0;
+30        int r=nums.length-1;
+31        int index=-1;
+32        while(l<=r){
+33            int mid=l+(r-l)/2;
+34
+35                if(nums[mid]==target){
+36                index=mid;
+37            }
+38            if(nums[mid]<=target){
+39                l=mid+1;
+40            }
+41            else{
+42                r=mid-1;
+43            }
+44        
+45        }
+46        return index;
+47    }
+48}
