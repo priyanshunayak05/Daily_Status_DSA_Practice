081419b4f206f028155bfbbdf39448a5acddd2f3
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-26 22:45:18 +0530

    File updated at 12/26/2025, 10:45:14 PM

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,13 +1,25 @@
-// Last updated: 10/7/2025, 3:48:34 PM
-class Solution {
-    public int minPairSum(int[] nums) {
-        int max=0;
-        int n=nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<n/2;i++){
-            int sum =nums[i]+nums[n-1-i];
-            max=Math.max(max,sum);
-        }
-        return max;
-    }
-}
+// Last updated: 12/26/2025, 10:45:14 PM
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3        long l=0;
+4        long r=100000000000000L;
+5        long ans=r;
+6        while(l<r){
+7            long mid=(l+r)/2;
+8            long count=0;
+9            for(int i=0;i<time.length;i++){
+10                count +=mid/time[i];
+11                
+12            }
+13            if(count >=totalTrips){
+14                    ans=Math.min(ans,mid);
+15                    r=mid;
+16                }
+17                else{
+18                    l=mid+1;
+19                }
+20        }
+21        return ans;
+22        
+23    }
+24}
