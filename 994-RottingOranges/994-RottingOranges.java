c0f6d926110a5805e2c0d685bfe32c193de708de
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-29 15:11:31 +0530

    File updated at 12/29/2025, 3:11:30 PM

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,61 +1,47 @@
-// Last updated: 11/15/2025, 9:33:52 PM
-class Pair{
-    int row;
-    int col;
-    int tm;
-    Pair(int row,int col,int tm){
-        this.row=row;
-        this.col=col;
-        this.tm=tm;
-    }
-}
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        Queue<Pair>q=new LinkedList<>();
-        int[][] vis=new int[n][m];
-        int cntfres=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=2;
-                }
-                else{
-                    vis[i][j]=0;
-                }
-                if(grid[i][j]==1){
-                    cntfres++;
-                }
-            }
-        }
-        int tm=0;
-        int[] drow={-1,0,1,0};
-        int []dcol={0,1,0,-1};
-
-        int cnt=0;
-        while(!q.isEmpty()){
-            int r=q.peek().row;
-            int c=q.peek().col;
-            int t=q.peek().tm;
-            tm=Math.max(t,tm);
-
-            q.remove();
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.add(new Pair(nrow,ncol,t+1));
-                    vis[nrow][ncol]=2;
-                    cnt++;
-                }
-            }
-        }
-
-        if(cnt!=cntfres)return -1;
-        return tm;
-        
-    }
-}
+// Last updated: 12/29/2025, 3:11:30 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int orangesRotting(int[][] grid) {
+5        Queue<int[]> q = new LinkedList<>();
+6        int fresh = 0;
+7
+8        // Step 1: collect rotten oranges & count fresh
+9        for (int i = 0; i < grid.length; i++) {
+10            for (int j = 0; j < grid[0].length; j++) {
+11                if (grid[i][j] == 2) q.offer(new int[]{i, j});
+12                if (grid[i][j] == 1) fresh++;
+13            }
+14        }
+15
+16        int minutes = 0;
+17        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+18
+19        // Step 2: BFS
+20        while (!q.isEmpty() && fresh > 0) {
+21            int size = q.size();
+22            minutes++;
+23
+24            for (int i = 0; i < size; i++) {
+25                int[] cell = q.poll();
+26
+27                for (int[] d : dirs) {
+28                    int r = cell[0] + d[0];
+29                    int c = cell[1] + d[1];
+30
+31                    if (r >= 0 && r < grid.length &&
+32                        c >= 0 && c < grid[0].length &&
+33                        grid[r][c] == 1) {
+34
+35                        grid[r][c] = 2;
+36                        fresh--;
+37                        q.offer(new int[]{r, c});
+38                    }
+39                }
+40            }
+41        }
+42
+43        return fresh == 0 ? minutes : -1;
+44    }
+45}
+46
