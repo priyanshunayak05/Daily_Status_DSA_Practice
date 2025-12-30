a503733a4770e8d43d5fbe8b9b80dcad4b4fb671
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-30 10:28:56 +0530

    File updated at 12/30/2025, 10:28:56 AM

--- 997-FindtheTownJudge/997-FindtheTownJudge.java
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -1,20 +1,20 @@
-// Last updated: 9/28/2025, 5:26:19 PM
-class Solution {
-    public int findJudge(int n, int[][] trust) {
-        int len=trust.length;
-        int out[]=new int[n+1];
-        int in[]=new int[n+1];
-
-        for(int i=0;i<len;i++){
-            out[trust[i][0]]++;
-            in[trust[i][1]]++;
-        }
-        //find the judge
-        for(int i=1;i<=n;i++){
-            if(out[i]==0 && in[i]==n-1){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
+// Last updated: 12/30/2025, 10:28:56 AM
+1class Solution {
+2    public int findJudge(int n, int[][] trust) {
+3        int len=trust.length;
+4        int out[]=new int[n+1];
+5        int in[]=new int[n+1];
+6
+7        for(int i=0;i<len;i++){
+8            out[trust[i][0]]++;
+9            in[trust[i][1]]++;
+10        }
+11        //find the judge
+12        for(int i=1;i<=n;i++){
+13            if(out[i]==0 && in[i]==n-1){
+14                return i;
+15            }
+16        }
+17        return -1;
+18    }
+19}
