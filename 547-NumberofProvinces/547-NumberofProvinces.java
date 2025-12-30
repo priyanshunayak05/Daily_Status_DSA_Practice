a5094bcdcf95f478f33953bd75108fbaed5a970d
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2025-12-30 22:31:22 +0530

    File updated at 12/30/2025, 10:31:21 PM

--- 547-NumberofProvinces/547-NumberofProvinces.java
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -1,25 +1,25 @@
-// Last updated: 10/30/2025, 5:58:34 PM
-class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        int prov=0;
-        boolean []visited=new boolean[n];
-
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(isConnected,visited,i);
-                prov++;
-            }
-        }
-        return prov;
-    }
-    public static void dfs(int [][]isConnected,boolean []visited,int i){
-        visited[i]=true;
-
-        for(int j=0;j<isConnected.length;j++){
-            if(isConnected[i][j]==1 && !visited[j]){
-                dfs(isConnected,visited,j);
-            }
-        }
-    }
-}
+// Last updated: 12/30/2025, 10:31:21 PM
+1class Solution {
+2    public int findCircleNum(int[][] isConnected) {
+3        int n=isConnected.length;
+4        int prov=0;
+5        boolean []visited=new boolean[n];
+6
+7        for(int i=0;i<n;i++){
+8            if(!visited[i]){
+9                dfs(isConnected,visited,i);
+10                prov++;
+11            }
+12        }
+13        return prov;
+14    }
+15    public static void dfs(int [][]isConnected,boolean []visited,int i){
+16        visited[i]=true;
+17
+18        for(int j=0;j<isConnected.length;j++){
+19            if(isConnected[i][j]==1 && !visited[j]){
+20                dfs(isConnected,visited,j);
+21            }
+22        }
+23    }
+24}
