ee345cdedc26b8914a9309baf738da7040cf4ed8
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-01 10:39:47 +0530

    File updated at 1/1/2026, 10:39:46 AM

--- 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
+++ 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
@@ -1,12 +1,12 @@
-// Last updated: 11/19/2025, 12:20:35 AM
-class Solution {
-    public int findCenter(int[][] edges) {
-        int a=edges[0][0];
-        int b=edges[0][1];
-
-        if(edges[1][0]==a || edges[1][1]==a){
-            return a;
-        }
-        return b;
-    }
-}
+// Last updated: 1/1/2026, 10:39:46 AM
+1class Solution {
+2    public int findCenter(int[][] edges) {
+3        int a=edges[0][0];
+4        int b=edges[0][1];
+5
+6        if(edges[1][0]==a || edges[1][1]==a){
+7            return a;
+8        }
+9        return b;
+10    }
+11}
