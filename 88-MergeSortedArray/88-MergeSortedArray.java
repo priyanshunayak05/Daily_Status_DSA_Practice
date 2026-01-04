ae28ddc2086b9b3424e42418e4e5a2739dd305ce
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-04 19:50:04 +0530

    File updated at 1/4/2026, 7:50:02 PM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,25 +1,9 @@
-// Last updated: 8/1/2025, 1:57:49 PM
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-            }
-            else{
-                nums1[k]=nums2[j];
-                j--;
-            }
-            k--;
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
-    
-    }
-}
+// Last updated: 1/4/2026, 7:50:02 PM
+1class Solution {
+2    public String largestEven(String s) {
+3        int idx=s.lastIndexOf('2');
+4        if(idx==-1)return "";
+5        return s.substring(0,idx+1);
+6
+7    }
+8}
