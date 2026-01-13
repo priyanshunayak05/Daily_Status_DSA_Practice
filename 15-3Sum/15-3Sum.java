23dd9434de1c3f31dbf788daa3077f3c166fb95c
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-13 11:25:31 +0530

    File updated at 1/13/2026, 11:25:31 AM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,29 +1,29 @@
-// Last updated: 9/26/2025, 5:15:40 PM
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>>set=new HashSet<>();
-        Arrays.sort(nums);
-        int n=nums.length;
-      
-    
-        for(int i=0;i<n-2;i++){
-            int j=i+1; int k=n-1;
-
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++;
-                    k--;
-                }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    k--;
-                }
-            }
-            }
-            return new ArrayList<>(set);
-    }
-}
+// Last updated: 1/13/2026, 11:25:31 AM
+1class Solution {
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        Set<List<Integer>>set=new HashSet<>();
+4        Arrays.sort(nums);
+5        int n=nums.length;
+6      
+7    
+8        for(int i=0;i<n-2;i++){
+9            int j=i+1; int k=n-1;
+10
+11            while(j<k){
+12                int sum=nums[i]+nums[j]+nums[k];
+13                if(sum==0){
+14                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+15                    j++;
+16                    k--;
+17                }
+18                else if(sum<0){
+19                    j++;
+20                }
+21                else{
+22                    k--;
+23                }
+24            }
+25            }
+26            return new ArrayList<>(set);
+27    }
+28}
