1d7837512f0761d0b1e948205a834aa15dba5d1f
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-21 12:11:16 +0530

    File updated at 1/21/2026, 12:11:15 PM

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,22 +1,19 @@
-// Last updated: 1/21/2026, 11:52:29 AM
+// Last updated: 1/21/2026, 12:11:15 PM
 1class Solution {
-2    public int xorAllNums(int[] nums1, int[] nums2) {
-3        int xor=0;
-4        int n=nums1.length;
-5        int m=nums2.length;
-6
-7        if(n%2!=0){
-8            for(int i:nums2){
-9                xor ^=i;
-10            }
-11       }
-12
-13       if(m%2!=0){
-14        for(int i:nums1){
-15            xor^=i;
-16        }
-17       }
-18       return xor;
-19
-20    }
-21}
+2    public int getXORSum(int[] arr1, int[] arr2) {
+3        int n=arr1.length;
+4        int m=arr2.length;
+5        int xor=0;
+6        int xor2=0;
+7        for(int i:arr1){
+8            xor ^=i;
+9        }
+10
+11        for(int i:arr2){
+12            xor2 ^=i;
+13        }
+14
+15        return xor & xor2;
+16        
+17    }
+18}
