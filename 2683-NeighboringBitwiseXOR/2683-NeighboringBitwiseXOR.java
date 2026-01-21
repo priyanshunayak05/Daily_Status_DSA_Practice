f2d78acc003db6dc3784959f1c7e282cf38b0892
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-21 11:52:31 +0530

    File updated at 1/21/2026, 11:52:29 AM

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,11 +1,22 @@
-// Last updated: 1/21/2026, 11:34:51 AM
+// Last updated: 1/21/2026, 11:52:29 AM
 1class Solution {
-2    public boolean doesValidArrayExist(int[] derived) {
+2    public int xorAllNums(int[] nums1, int[] nums2) {
 3        int xor=0;
-4        for(int v:derived){
-5            xor ^=v;
+4        int n=nums1.length;
+5        int m=nums2.length;
 6
-7        }
-8        return xor ==0;
-9    }
-10}
+7        if(n%2!=0){
+8            for(int i:nums2){
+9                xor ^=i;
+10            }
+11       }
+12
+13       if(m%2!=0){
+14        for(int i:nums1){
+15            xor^=i;
+16        }
+17       }
+18       return xor;
+19
+20    }
+21}
