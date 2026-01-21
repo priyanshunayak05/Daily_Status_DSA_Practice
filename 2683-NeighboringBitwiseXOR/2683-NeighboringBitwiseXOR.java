0d4580c10abb2699966c9be9707f491d8b0bfc0b
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-21 12:39:01 +0530

    File updated at 1/21/2026, 12:38:59 PM

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,19 +1,35 @@
-// Last updated: 1/21/2026, 12:11:15 PM
+// Last updated: 1/21/2026, 12:38:59 PM
 1class Solution {
-2    public int getXORSum(int[] arr1, int[] arr2) {
-3        int n=arr1.length;
-4        int m=arr2.length;
-5        int xor=0;
-6        int xor2=0;
-7        for(int i:arr1){
-8            xor ^=i;
-9        }
-10
-11        for(int i:arr2){
-12            xor2 ^=i;
-13        }
-14
-15        return xor & xor2;
-16        
-17    }
-18}
+2    public int minimizeXor(int num1, int num2) {
+3        int set_bit=0;
+4        while(num2>0){
+5            num2=(num2&(num2-1));
+6            set_bit++;
+7        }
+8        int x=0;
+9        for(int i=30;i>=0;i--){
+10            int mask=(1<<i);
+11            if((num1 & mask)!=0){
+12                x|=mask;
+13                set_bit--;
+14                if(set_bit==0){
+15
+16                    return x;
+17                }
+18            }
+19        }
+20
+21            for(int i=0;i<=30;i++){
+22                int mask=(1 <<i);
+23                if((num1 & mask)==0){
+24                    x|=mask;
+25                    set_bit --;
+26                    if(set_bit==0){
+27                        return x;
+28                    }
+29                }
+30            }
+31            return x;
+32        
+33    }
+34}
