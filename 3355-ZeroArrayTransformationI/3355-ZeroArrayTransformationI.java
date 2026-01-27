596d1a9be5ded90040c91536af35d0869b27d9d0
Priyanshu Nayak <142567587+priyanshunayak05@users.noreply.github.com>
2026-01-27 14:43:41 +0530

    File updated at 1/27/2026, 2:43:41 PM

--- 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.java
+++ 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.java
@@ -1,55 +1,69 @@
-// Last updated: 1/27/2026, 2:29:29 PM
+// Last updated: 1/27/2026, 2:43:41 PM
 1class MyCircularQueue {
 2    int size;
-3    int cnt;
-4    Queue<Integer> q;
-5    public MyCircularQueue(int k) {
-6        size=k;
-7        cnt=0;
-8        q=new LinkedList<>();  //to store data 
-9    }
-10    
-11    public boolean enQueue(int value) {
-12        if(cnt==size)return false;
-13        cnt++;
-14        q.add(value);
-15        return true;
-16    }
-17    
-18    public boolean deQueue() {
-19        if(cnt==0) return false;
-20        q.poll();
-21        cnt--;
-22        return true;
-23    }
-24    
-25    public int Front() {
-26        if(cnt==0) return -1;
-27        return q.peek();
-28    }
-29    
-30    public int Rear() {
-31        if(cnt==0) return -1;
-32        LinkedList<Integer> list = (LinkedList<Integer>) q;
-33        return list.getLast();
-34    }
-35    
-36    public boolean isEmpty() {
-37        return cnt==0;
-38    }
-39    
-40    public boolean isFull() {
-41        return cnt==size;
-42    }
-43}
-44
-45/**
-46 * Your MyCircularQueue object will be instantiated and called as such:
-47 * MyCircularQueue obj = new MyCircularQueue(k);
-48 * boolean param_1 = obj.enQueue(value);
-49 * boolean param_2 = obj.deQueue();
-50 * int param_3 = obj.Front();
-51 * int param_4 = obj.Rear();
-52 * boolean param_5 = obj.isEmpty();
-53 * boolean param_6 = obj.isFull();
-54 */
+3    int count;
+4    Queue<Integer>q;
+5
+6    public MyCircularQueue(int k) {
+7        size=k;
+8        count=0;
+9        q=new LinkedList<>();
+10        
+11    }
+12    
+13    public boolean enQueue(int value) {
+14        if(count==size)return false;
+15        count++;
+16        q.add(value);
+17        return true;
+18        
+19    }
+20    
+21    public boolean deQueue() {
+22        if(count==0){
+23            return false;
+24        }
+25        count--;
+26        q.poll();
+27        return true;
+28        
+29    }
+30    
+31    public int Front() {
+32        if(count==0)return -1;
+33
+34        return q.peek();
+35        
+36    }
+37    
+38    public int Rear() {
+39        if (count==0)return -1;
+40        LinkedList<Integer> list = (LinkedList<Integer>) q;
+41        return list.getLast();
+42
+43        
+44    }
+45    
+46    public boolean isEmpty() {
+47        return count==0;
+48        
+49    }
+50    
+51    public boolean isFull() {
+52        return count==size;
+53
+54
+55        
+56    }
+57}
+58
+59/**
+60 * Your MyCircularQueue object will be instantiated and called as such:
+61 * MyCircularQueue obj = new MyCircularQueue(k);
+62 * boolean param_1 = obj.enQueue(value);
+63 * boolean param_2 = obj.deQueue();
+64 * int param_3 = obj.Front();
+65 * int param_4 = obj.Rear();
+66 * boolean param_5 = obj.isEmpty();
+67 * boolean param_6 = obj.isFull();
+68 */
